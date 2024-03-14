282758d0178ff7935717c6b1d0937abd51045fbc
Joachim Koenig <joakoeni@k0r1p14.42mulhouse.fr>
2024-03-14 16:20:14 +0100

    init

---
+++ ex00/Class/BitcoinExchange/BitcoinExchange.cpp
@@ -0,0 +1,17 @@
+
+
+// ----------Constructors----------
+
+// ----------Deconstructors----------
+
+// ----------Setters----------
+
+// ----------Getters----------
+
+// ----------Operators_Overloaders----------
+
+// ----------Members_Functions----------
+
+// ----------Non_Members_Functions----------
+
+
---
+++ ex00/Class/BitcoinExchange/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+// 1 = parse csv stock in container
+// 2 = display it  with good form= value multiplied by the exchange rate according to the date indicated
+
+#include <iostream>
+
+class BitcoinExchange
+{
+	private:
+
+	public:
+};
+
+#endif
