00179ef65da4e93b7a94934d6db7fdf5cd089a81
Joachim Koenig <joakoeni@k2r4p1.42mulhouse.fr>
2024-03-18 10:05:37 +0100

    init

--- ex00/Class/BitcoinExchange/BitcoinExchange.cpp
+++ ex00/Class/BitcoinExchange/BitcoinExchange.cpp
@@ -1,8 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joakoeni <joakoeni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 09:46:11 by joakoeni          #+#    #+#             */
+/*   Updated: 2024/03/18 10:05:22 by joakoeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "./BitcoinExchange.hpp"
 
 // ----------Constructors----------
 
+BitcoinExchange::BitcoinExchange()
+{
+	return;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src)
+{
+	return;
+}
+
 // ----------Deconstructors----------
+
+BitcoinExchange::~BitcoinExchange()
+{
+	return;
+}
 
 // ----------Setters----------
 
--- ex00/Class/BitcoinExchange/BitcoinExchange.hpp
+++ ex00/Class/BitcoinExchange/BitcoinExchange.hpp
@@ -1,16 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joakoeni <joakoeni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 09:46:14 by joakoeni          #+#    #+#             */
+/*   Updated: 2024/03/18 09:46:16 by joakoeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
 // 1 = parse csv stock in container
 // 2 = display it  with good form= value multiplied by the exchange rate according to the date indicated
 
+#include <exception>
 #include <iostream>
+#include <map>
 
 class BitcoinExchange
 {
 	private:
-
+		const std::map<const std::string, const float> CsvParsed;
 	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &src);
+		~BitcoinExchange();
+		BitcoinExchange&	operator=(const BitcoinExchange& src);
+		const 				std::string getLine() const;
+		const				std::string splitLineDate(const std::string Line) const;
+		const				float splitLineValue(const std::string Line) const;
+		void				addData(const std::string& Date, const float Value);
+	
+	class NoArgFileException : public std::exception
+	{
+		public:
+			const std::string err() const throw();
+	};
+	class BadLineFormatException : public std::exception
+	{
+		public:
+			const std::string err() const throw();
+	};
+	class BadDateFormatException : public std::exception
+	{
+		public:
+			const std::string err() const throw();
+	};
+	class BadValueException : public std::exception
+	{
+		public:
+			const std::string err() const throw();
+	};
 };
 
 #endif
